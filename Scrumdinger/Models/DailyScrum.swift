aa33aabba0ab5f009d7b08cc31d5736ddec43e10
Jaimin Parmar <jaimins100@gmail.com>
2025-12-02 11:59:08 +0530

    import themekit and create dailyscrum structure

---
+++ Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,16 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by Jaimin on 02/12/25.
+//
+
+import Foundation
+import ThemeKit
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
