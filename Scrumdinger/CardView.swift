c4a9a6072ae40e44ed3649867224092db22fde14
Jaimin Parmar <jaimins100@gmail.com>
2025-12-07 16:43:05 +0530

    create cardview

---
+++ Scrumdinger/CardView.swift
@@ -0,0 +1,35 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by Jaimin on 06/12/25.
+//
+
+import SwiftUI
+import ThemeKit
+
+struct CardView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+            Spacer()
+            HStack {
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock").padding(.trailing, 20)
+            }
+            .font(.caption)
+        }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
+    }
+}
+
+#Preview(traits: .fixedLayout(width: 400, height: 60)) {
+    let scrum = DailyScrum.sampleData[0]
+    return CardView(scrum: scrum)
+        .background(scrum.theme.mainColor)
+}
