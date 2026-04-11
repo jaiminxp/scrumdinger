c9b5582961b1528d2311a2589996b5b69d96f316
Jaimin Parmar <jaimins100@gmail.com>
2026-04-11 16:05:16 +0530

    Add a theme picker

---
+++ Scrumdinger/Views/ThemePicker.swift
@@ -0,0 +1,27 @@
+//
+//  ThemePicker.swift
+//  Scrumdinger
+//
+//  Created by Jaimin on 11/04/26.
+//
+
+import SwiftUI
+import ThemeKit
+
+struct ThemePicker: View {
+    @Binding var selection: Theme
+    var body: some View {
+        Picker("Theme", selection: $selection) {
+            ForEach(Theme.allCases) { theme in
+                ThemeView(theme: theme)
+                    .tag(theme)
+            }
+        }
+        .pickerStyle(.navigationLink)
+    }
+}
+
+#Preview {
+    @Previewable @State var theme = Theme.periwinkle
+    ThemePicker(selection: $theme)
+}
